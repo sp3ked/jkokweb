115dcbb58c2d1412e40aacce44f77a4ed831b2af
KrusherK77 <Jk4thefox@gmail.com>
2024-06-08 23:09:11 -0400

    starting format

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload asfdoasf.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="sidebar">
+        <img src="/path_to_your_profile_picture.jpg" alt="Profile" className="profile-pic" />
+        <h1>Your Name</h1>
+        <a href="https://github.com/sp3cked" target="_blank" rel="noopener noreferrer">GitHub</a>
+        <a href="https://twitter.com/sp3cked" target="_blank" rel="noopener noreferrer">Twitter</a>
+      </div>
+      <div className="content">
+        <div className="box">
+          <div className="rectangle"></div>
+          <p>Information about cars...</p>
+        </div>
+        <div className="box">
+          <div className="rectangle"></div>
+          <p>Information about cars...</p>
+        </div>
+        <div className="box">
+          <div className="rectangle"></div>
+          <p>Information about cars...</p>
+        </div>
+        <div className="box">
+          <div className="rectangle"></div>
+          <p>Information about cars...</p>
+        </div>
+      </div>
     </div>
   );
 }
