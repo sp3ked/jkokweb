5420f7c7ed3f2638f119a5e3a5b59429214cdd29
KrusherK77 <Jk4thefox@gmail.com>
2024-06-22 00:53:32 -0400

    GOOD NIGHT

--- src/App.js
+++ src/App.js
@@ -6,16 +6,16 @@ import Projects from "./Projects";
 function App() {
   return (
     <div className="container">
-      <div className="sidebar-container">
-        <div className="sidebar">
-          <img src="/path_to_your_profile_picture.jpg" alt="Profile" className="profile-pic" />
-          <h1>Your Name</h1>
-          <a href="https://github.com/sp3ked" target="_blank" rel="noopener noreferrer">GitHub</a>
-          <a href="https://twitter.com/sp3ked" target="_blank" rel="noopener noreferrer">Twitter</a>
-        </div>
+      <div className="Profile">
+        <img src="/path_to_your_profile_picture.jpg" alt="Profile" className="profile-pic" />
+        <h1>Your Name</h1>
+        <a href="https://github.com/sp3ked" target="_blank" rel="noopener noreferrer">GitHub</a>
+        <a href="https://twitter.com/sp3ked" target="_blank" rel="noopener noreferrer">Twitter</a>
       </div>
-      <div className="main-content">
+      <div className="Navbar">
         <Navbar />
+      </div>
+      <div className="Content">
         <Projects />
       </div>
     </div>
--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,14 +1,24 @@
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./Navbar.css";
+import Navbar from "./Navbar";
+import Projects from "./Projects";
+import About from "./About";
+import Resume from "./Resume"; 
 
-function Navbar() {
+function App() {
   return (
-    <div className="navbar">
-      <a href="#about">About</a>
-      <a href="#projects">Projects</a>
-      <a href="#resume">Resume</a>
-    </div>
+    <BrowserRouter>
+      <div className="container">
+        <Navbar />
+        <Routes>
+          <Route path="/about" element={<About />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/resume" element={<Resume />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
-export default Navbar;
+export default App;
