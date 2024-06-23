afbec019997281caa8ee703aca4ece114b3a63ad
KrusherK77 <Jk4thefox@gmail.com>
2024-06-23 13:12:23 -0400

    left nav done

--- src/Pages/leftnav.jsx
+++ src/Pages/leftnav.jsx
@@ -1,13 +1,31 @@
 import React from "react";
 import "./leftnav.css";
+import profilePic from '../images/pfp.png';
+import githubIcon from '../images/github.svg'; 
+import twitterIcon from '../images/twitter.svg';
+import linkedinIcon from '../images/linkedin.svg'; 
 
 function LeftNav() {
     return (
         <div className="Profile">
-            <img src="/path_to_your_profile_picture.jpg" alt="Profile" className="profile-pic" />
-            <h1>Your Name</h1>
-            <a href="https://github.com/sp3ked" target="_blank" rel="noopener noreferrer">GitHub</a>
-            <a href="https://twitter.com/sp3ked" target="_blank" rel="noopener noreferrer">Twitter</a>
+            <div>
+                <div className="circular--landscape">
+                    <img src={profilePic} alt="Profile" className="profile-pic" />
+                </div>
+                <h1>Joey Kokinda</h1>
+                <p className="description">Student studying AI at Purdue University</p>
+            </div>
+            <div className="links">
+                <a href="https://github.com/sp3ked" target="_blank" rel="noopener noreferrer"> {/* opens new tab  */} 
+                    <img src={githubIcon} alt="GitHub" />
+                </a>
+                <a href="https://twitter.com/sp3ked" target="_blank" rel="noopener noreferrer">
+                    <img src={twitterIcon} alt="Twitter" />
+                </a>
+                <a href="https://www.linkedin.com/in/jkokinda/" target="_blank" rel="noopener noreferrer">
+                    <img src={linkedinIcon} alt="LinkedIn" />
+                </a>
+            </div>
         </div>
     );
 }
