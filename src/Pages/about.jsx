d057c19c29357d838346be39948f6b3466214a04
spek <jkokinda9@gmail.com>
2024-11-07 20:41:23 -0500

    english bruh

--- src/Pages/about.jsx
+++ src/Pages/about.jsx
@@ -14,8 +14,8 @@ function About() {
         </p>
         <p>
           Ive been wokring on many different things in the past few years, from hardware projects to seoftware projects.
-          One example can be found below where me and my team made an Ai powered pacman
-          robot to play in a real life game of pacman. (The cad file can be
+          One example of a hardware project ive done is when me and my team made an Ai powered pacman
+          robot to play in a real life game of pacman (The cad file can be
           found{" "}
           <a
             href="https://cad.onshape.com/documents/1a384639168640173528f35e/w/297262e1ee6944cdbbdc1550/e/1d1cce23977b581de6f023e4?renderMode=0&uiState=669924c9f63dd019d4336e48"
@@ -23,9 +23,11 @@ function About() {
             rel="noopener noreferrer"
             className="underline-link"
           >
-            here
+          here
           </a>
-          ). On the other side of things in software ive worked on many differnet projects from online casino websites to mobile apps. On the other side of this stuff, I also engage in
+          
+          ). This is just one example, others can be found in my projects tab (bike conversion, smart glasses, etc.)
+          In software ive worked on many differnet projects from online casino websites to mobile apps, see exact deatils in my projects tab. On the other side of this stuff, I also engage in
           activities like flag football, spikeball, and basketball.
         </p>
         <p>
