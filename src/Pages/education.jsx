51d62847e244aacb956a4861295698a428817132
spek <jkokinda9@gmail.com>
2025-03-15 16:07:05 -0400

    education

--- src/Pages/education.jsx
+++ src/Pages/education.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import purdueLogo from "../images/purdue.png";
+import ritLogo from "../images/rit.png"; // Import the RIT logo
 import "./education.css";
 
 function Education() {
@@ -16,6 +17,7 @@ function Education() {
       </div>
 
       <div className="educationContent">
+        {/* Purdue University Education */}
         <div className="education-item">
           <div className="education-logo">
             <img src={purdueLogo} alt="Purdue University logo" />
@@ -29,6 +31,25 @@ function Education() {
             <p className="education-activities">
               <strong>Activities:</strong> Boiler Blockchain Club (dev team), Purdue Hackers, 
               ML@Purdue, and Intramural (Basketball, Wiffleball, Ultimate Frisbee).
+            </p>
+          </div>
+        </div>
+
+        {/* Rochester Institute of Technology Education */}
+        <div className="education-item">
+          <div className="education-logo">
+            <img src={ritLogo} alt="Rochester Institute of Technology logo" />
+          </div>
+          <div className="education-info">
+            <h3>Rochester Institute of Technology</h3>
+            <span className="education-degree">
+              Bachelor's degree, Robotics
+            </span>
+            <span className="education-duration">2023 - 2024</span>
+            <p className="education-activities">
+              <strong>Activities and societies:</strong> MDRC (Multi-Disciplinary Robotics Club, Pacbot), 
+              Artificial Intelligence Club, Business and Technology Professionals Club, 
+              Flag Football, Intramural Basketball
             </p>
           </div>
         </div>
